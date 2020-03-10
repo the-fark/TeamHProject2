91aa934a65a9369d8e1a8f7367da64d98e606258
getusername67 <61632919+getusername67@users.noreply.github.com>
2020-03-10 13:54:04 -0500

    Update Main.java

--- TeamHProject2/src/Main.java
+++ TeamHProject2/src/Main.java
@@ -7,40 +7,57 @@ import java.util.Scanner;
 
 public class Main {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
 		
-		//The next two lines describes the program to the user
+		//The next two lines describe the program to the user
 		System.out.println("This program is a coin flipping game. The goal is to get exactly one dollar. You lose if the number goes above one dollar.");
 		System.out.println("A penny, a nickel, a dime, and a quarter will all be flipped simultaneously when you press enter to flip the coins.");
 		
+		//These initialize the variables for the users response to whether they would like to repeat the game or not
 		char repeat;
 		String input;
+		
+		//Creates a keyboard object
 		Scanner keyboard = new Scanner(System.in);
+		
+		//This loop will run once then ask the user if they would like to repeat the program after every loop
 		do {
 			
+			//These create objects from the coin classes
 			penny p = new penny();
 			nickel n = new nickel();
 			dime d = new dime();
 			quarter q = new quarter();
 			
+			//Initializes the balance as 0
 			double balance=0;
 			
+			//Initializes the coin variables as double
 			double penny, nickel, dime, quarter;
 			
+			//This loop will flip coins until the balance has reached at least $1.00
 			while(balance<1) {
+				
+				//This shows the user that the starting balance is 0, and that they need to enter any key to flip the coins again
 				System.out.println("Balance: $"+String.format("%.2f", balance));
 				System.out.println("Press any key to flip again");
+				
+				//Gets the response from the user
 				keyboard.nextLine();
+				
+				//Makes a coin variable to hold the value from its respective method
 				penny = p.pennyM();
-				nickel = n.nickelM();
+				nickel = n.nickelM();to
 				dime = d.dimeM();
 				quarter = q.quarterM();
-				
+			
+				//Accumulates the values of the coins that land on heads
 				balance += penny;
 				balance += nickel;
 				balance += dime;
 				balance += quarter;
 				
+				//These next four if/else statements show the user whether each coin landed on heads or tails
 				if (penny == 0.01) {
 					System.out.println("Penny: Heads");
 				}else {
@@ -64,23 +81,30 @@ public class Main {
 					
 				}
 			
+			//Tells the user that they have won the game if they got a balance of $1.00
 			if(balance==1) {
 				System.out.println("Balance: $"+String.format("%.2f", balance));
 				System.out.println("Congratulations, you win!");
-			}else {
+			}
+			//Tells the user that they have lost the game if they got a balance over $1.00
+			else {
 				System.out.println("Balance: $"+String.format("%.2f", balance));
 				System.out.println("Oof, better luck next time!");
 			}
+			
+			//This asks the user if they would like to repeat the game
 			System.out.println("Would you like to play this game again? Enter 'Y' or 'y' to repeat, or type anything else to end this program.");
 			input = keyboard.nextLine();
+			//Validates the input in case the user doesn't enter anything
 			while (input.equalsIgnoreCase("")){
 				System.out.println("The response you entered is invalid. If you would like to play this game again, enter 'Y' or 'y'");
 				input = keyboard.nextLine();
 			}
 			repeat = input.charAt(0);
-			
-		}while (repeat == 'Y' || repeat == 'y');
-		System.out.println("Thank you playing this game!");
+		}
+		//Repeats the program when the user enters 'Y' or 'y'
+		while (repeat == 'Y' || repeat == 'y');
+		System.out.println("Thank you for playing this game!");
 		keyboard.close();
 	}
 }
